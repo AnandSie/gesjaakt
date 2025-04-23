fd73de06e98a6648339b8d3d5525777992ba4fa1
Anand Sie <anand.sie@cgi.com>
2025-04-23 14:05:04 +0200

    Random order players every game

---
+++ Src/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Extensions
+{
+    public static class EnumerableExtensions
+    {
+        private static Random Random = new();
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.OrderBy(_ => Random.Next());
+        }
+    }
+}
