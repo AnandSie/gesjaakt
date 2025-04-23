6581a6f918a709758814d30cad866bfc85c6d371
marijn.brandwijk <marijn.brandwijk@cgi.com>
2025-04-23 11:37:22 +0200

    test

---
+++ Src/Domain/Entities/Thinkers/MarijnThinker.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities.Players;
+using Domain.Interfaces;
+
+namespace Domain.Entities.Thinkers;
+
+public class MarijnThinker : IThinker
+{
+    public TurnAction Decide(IGameStateReader _)
+    {
+        return TurnAction.SKIPWITHCOIN;
+    }
+}
