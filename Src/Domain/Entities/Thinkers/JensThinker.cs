908b4ff45e7f93c52fc12bbeb0476b616b84fc39
jens.meinderts <jens.meinderts@prorail.nl>
2025-04-23 12:08:23 +0200

    bot

--- Src/Domain/Entities/Thinkers/JensThinker.cs
+++ Src/Domain/Entities/Thinkers/JensThinker.cs
@@ -1,22 +1,26 @@
 ﻿using Domain.Entities.Players;
 using Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Entities.Thinkiers;
 
 public class JensThinker : IThinker
 {
+    private const int rondjesTippingPoint = 1;
+
+    private bool isMelking = false;
+    private int rondjes = 0;
+
     public TurnAction Decide(IGameStateReader gameState)
     {
-        var tippingPoint = 15;
+        var tippingPoint = 14;
         int nettoPenaltyPoints = gameState.OpenCardValue - gameState.AmountOfCoinsOnTable;
-        var perceivedCoinValue = gameState.AmountOfCoinsOnTable * 3;
+        var perceivedCoinValue = gameState.AmountOfCoinsOnTable * 2.7;
 
         if (nettoPenaltyPoints < tippingPoint || perceivedCoinValue > gameState.OpenCardValue)
+        {
+            return TurnAction.TAKECARD;
+        }
+        else if(IsInStraatjeAndLangGenoegGemolken(gameState))
         {
             return TurnAction.TAKECARD;
         }
@@ -24,5 +28,28 @@ public class JensThinker : IThinker
         {
             return TurnAction.SKIPWITHCOIN;
         }
+    }
+
+    private bool IsInStraatjeAndLangGenoegGemolken(IGameStateReader gameState)
+    {
+        foreach (var card in gameState.PlayerOnTurn.Cards)
+        {
+            var diff = Math.Abs(card.Value - gameState.OpenCardValue);
+            if (diff == 1)
+            {
+                // TODO: Extract to separete method
+                if (isMelking && rondjes == rondjesTippingPoint)
+                {
+                    isMelking = false;
+                    return true;
+                }
+                else
+                {
+                    rondjes += 1;
+                    isMelking = true;
+                }
+            }
+        }
+        return false;
     }
 }
