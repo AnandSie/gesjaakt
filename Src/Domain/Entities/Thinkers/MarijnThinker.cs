61a6efb682fcc2c63b08acdf794efe98c4e90628
marijn.brandwijk <marijn.brandwijk@cgi.com>
2025-04-25 16:11:12 +0200

    tuned parameters

--- Src/Domain/Entities/Thinkers/MarijnThinker.cs
+++ Src/Domain/Entities/Thinkers/MarijnThinker.cs
@@ -1,6 +1,4 @@
-﻿using Domain.Entities.Cards;
-using Domain.Entities.Game;
-using Domain.Entities.Players;
+﻿using Domain.Entities.Players;
 using Domain.Interfaces;
 
 namespace Domain.Entities.Thinkers;
@@ -13,27 +11,21 @@ public class MarijnThinker : IThinker
         var tableCoins = gameState.AmountOfCoinsOnTable;
         var dealStrength = gameState.OpenCardValue - tableCoins;
 
-        var thresholdMyCoins = 5; //coins
-        var minTableThreshold = 5;
-        var maxTableThreshold = 15;
-        var dealT1 = 5;
-        var dealT2 = 10;
+        var thresholdMyCoins = 5; 
+        var tableThreshold1 = 5;
+        var tableThreshold2 = 10;
+        var dealT1 = 0;
+        var dealT2 = 15;
 
-        var wMyCoins = 0.08; // 0.34;
-        var wCoinsOnTable = 0.41;
-        var wDealStrength = 0.59;
+        var wMyCoins = 0.05; 
+        var wCoinsOnTable = 0.90;
+        var wDealStrength = 0.05;
 
         var pMyCoins = myCoins < thresholdMyCoins ? (1 / Math.Pow(thresholdMyCoins, 2)) * Math.Pow(myCoins - thresholdMyCoins, 2) : 0;
-        var pCoinsOnTable = tableCoins < minTableThreshold ? 0 : tableCoins > maxTableThreshold ? 1 : tableCoins - 5;
+        var pCoinsOnTable = tableCoins < tableThreshold2 ? 0 : tableCoins > tableThreshold1 ? 1 : (1 / (tableThreshold2 - tableThreshold1)) * dealStrength - (tableThreshold1 / (tableThreshold2 - tableThreshold1));  //tableCoins - 5;
         var pDealStrength = dealStrength <= dealT1 ? 1 : dealStrength > dealT2 ? 0 : (1/(dealT1-dealT2))*dealStrength -(dealT2/(dealT1-dealT2));
 
-        var pTotal = pCoinsOnTable;// Math.Pow((wMyCoins * pMyCoins) * (wCoinsOnTable * pCoinsOnTable) * (wDealStrength * pDealStrength), 1 / 3.0);
-
-        //Wins from ananad    //Math.Pow((wMyCoins * pMyCoins) * (wCoinsOnTable * pCoinsOnTable) * (wDealStrength * pDealStrength), 1 / 3.0);
-
-        //pMyCoins + pCoinsOnTable + pDealStrength; // wins from local maarten
-
-        //Math.Pow((wMyCoins * pMyCoins) * (wCoinsOnTable* pCoinsOnTable) * (wDealStrength* pDealStrength), 1 / 3.0);
+        var pTotal = (wMyCoins * pMyCoins) + (wCoinsOnTable * pCoinsOnTable) + (wDealStrength * pDealStrength);
 
         var random = new Random();
         if (random.NextDouble() < pTotal)
